d4ff7a1cca012af64e452a0b95e6c91d99ee4b77
Vinicius Gomes <gowesvini@gmail.com>
2021-01-09 12:33:17 -0300

    Final project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+import ReactMarkdown from 'react-markdown'
+import gfm from 'remark-gfm'
+
+import ReactTooltip from "react-tooltip";
+
+import './styles/global.css';
+import './styles/master.css';
+
+export default function App() {
+  const [text, setText] = useState('');
+
+  useEffect(() => {
+    const localStorageText = localStorage.getItem('@markdown-app/text');
+
+    if (localStorageText) {
+      setText(localStorageText);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('@markdown-app/text', text);
+  }, [text]);
+
+  function handleCopyTextToClipboard() {
+    navigator.clipboard.writeText(text);
+
+    alert('Copied!');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <ReactTooltip id="copyTip" place="bottom" effect="solid">
+        Click here to copy the text!
+      </ReactTooltip>
+
+      <textarea
+        placeholder="Enter here your markdown text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        className="input-area"
+      />
+
+      <button
+        data-tip data-for="copyTip"
+        className="copy-button"
+        type="button"
+        onClick={handleCopyTextToClipboard}
+      >
+        Copy
+      </button>
+
+      <ReactMarkdown
+        plugins={[gfm]}
+        className="markdown-container"
+        children={text}
+      />
     </div>
   );
 }
-
-export default App;
